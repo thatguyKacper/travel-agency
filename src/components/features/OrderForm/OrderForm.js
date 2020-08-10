062e88837b19a03cb286a9854a6e0603caa8ee1a
Kacper Wójtowicz <kacper.m.wojtowicz@gmial.com>
2020-08-10 16:34:53 +0200

    19.4

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,8 +5,49 @@ import { Row, Col } from 'react-flexbox-grid';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
 import styles from './OrderForm.scss';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+import Button from '../../common/Button/Button';
 
-const OrderForm = ({ options, tripCost, setOrderOption }) => {
+const sendOrder = (options, tripCost, tripName, tripId, countryCode) => {
+
+  if (options.name !== '' && options.contact !== '') {
+
+    const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+    const payload = {
+      ...options,
+      totalCost,
+      tripName,
+      tripId,
+      countryCode,
+    };
+
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, fetchOptions)
+      .then(function (response) {
+        return response.json();
+      }).then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
+  else {
+    alert('Name and contact cant be empty!');
+  }
+};
+
+const OrderForm = ({ options, tripCost, setOrderOption, tripId, tripName, countryCode }) => {
   return (
     <Row className={styles.components}>
       {pricing.map(option => (
@@ -17,6 +58,7 @@ const OrderForm = ({ options, tripCost, setOrderOption }) => {
       )}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} options={options} />
+        <Button onClick={() => sendOrder(options, tripCost, tripId, tripName, countryCode)}>Order now!</Button>
       </Col>
     </Row>
   );
@@ -26,6 +68,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripName: PropTypes.string,
+  tripId: PropTypes.string,
+  countryCode: PropTypes.string,
 
 };
 
