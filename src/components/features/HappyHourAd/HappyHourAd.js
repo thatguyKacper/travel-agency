076c442319ac82f765c2ff52425c23b1dbb7ae42
Kacper Wójtowicz <kacper.m.wojtowicz@gmial.com>
2020-08-27 17:18:02 +0200

    20.2

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './HappyHourAd.scss';
+
+class HappyHourAd extends Component {
+  constructor() {
+    super();
+
+    /* run this.forceUpdate() every second */
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+  }
+
+  static propTypes = {
+    title: PropTypes.string,
+    promoDescription: PropTypes.string,
+  }
+
+  getCountdownTime() {
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
+    }
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
+  }
+
+  render() {
+    const { title, promoDescription } = this.props;
+    const countTime = this.getCountdownTime();
+
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{title}</h3>
+        {countTime > 82800
+          ? (
+            <div className={styles.promoDescription}>{promoDescription}</div>)
+          : (
+            <div className={styles.promoDescription}>{this.getCountdownTime()}</div>
+          )}
+      </div>
+    );
+  }
+}
+
+export default HappyHourAd;
